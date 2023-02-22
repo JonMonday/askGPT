433bfb9fa5b4cd58d230716ce634a3ed129aabaa
JohnMonday <79201337+JonMonday@users.noreply.github.com>
2023-02-22 16:32:43 +0000

    Update script.js

--- client/script.js
+++ client/script.js
@@ -74,7 +74,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv)
 
   //fetch response from server
-  const response = await fetch('https://askgpt-mkz1.onrender.com/', {
+  const response = await fetch('https://askgpt-mkz1.onrender.com', {
     method: 'POST', 
     headers: {
       'Content-Type': 'application/json'
